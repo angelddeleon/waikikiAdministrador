31e890831659c8ccae112bafdaeaf5517e48f892
angelddeleon <angeldeleonpr00@gmail.com>
2025-04-14 20:38:33 -0400

    arreglos

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ class Config:
     # Clave secreta para proteger sesiones
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mi_clave_secreta'
     
-    # Configuración de la base de datos (MySQL)
+    # Configuración de la base de datos (MySQL con PyMySQL)
     MYSQL_USER = 'waikikip_admin'
     MYSQL_PASSWORD = 'K(ivE][]T1{b'
-    MYSQL_HOST = 'localhost'  # O la IP de tu servidor MySQL
-    MYSQL_PORT = 3306  # El puerto por defecto de MySQL
+    MYSQL_HOST = 'localhost'
+    MYSQL_PORT = 3306
     MYSQL_DB = 'waikikip_waikiki'
     
-    # Cadena de conexión para SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
+    # Cadena de conexión modificada para usar PyMySQL
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
